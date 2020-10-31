68519b052824058c5ab0ab5e323a2832799f3636
snebotcifpfbmoll <snebot@cifpfbmoll.eu>
2020-10-31 11:07:32 +0100

    task 3

--- app/src/main/java/com/serafinebot/dint/scrollingtext/MainActivity.java
+++ app/src/main/java/com/serafinebot/dint/scrollingtext/MainActivity.java
@@ -1,8 +1,13 @@
 package com.serafinebot.dint.scrollingtext;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.ViewSwitcher;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button addComment = findViewById(R.id.edit_comment);
+        addComment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ViewSwitcher vs = findViewById(R.id.view_switcher);
+                View current = vs.getCurrentView();
+
+                if (current instanceof EditText) {
+                    EditText editArticle = findViewById(R.id.edit_article);
+                    TextView article = findViewById(R.id.article);
+                    article.setText(editArticle.getText());
+
+                    Button button = findViewById(R.id.edit_comment);
+                    button.setText("EDIT COMMENT");
+                } else if (current instanceof TextView) {
+                    TextView article = findViewById(R.id.article);
+                    EditText editArticle = findViewById(R.id.edit_article);
+                    editArticle.setText(article.getText());
+                    Button button = findViewById(R.id.edit_comment);
+                    button.setText("SAVE COMMENT");
+                } else {
+                    return;
+                }
+
+                vs.showNext();
+            }
+        });
     }
 }
